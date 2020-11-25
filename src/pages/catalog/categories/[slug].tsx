60106f31a05a2311b15241800e6e26797eaf7ce5
ian.bertoncelo <ian.bertoncelo@infracommerce.com.br>
2020-11-25 09:37:02 -0300

    Add dynamic static pages

---
+++ src/pages/catalog/categories/[slug].tsx
@@ -0,0 +1,67 @@
+import {GetStaticPaths, GetStaticProps} from 'next'
+import {useRouter} from 'next/router'
+
+import api from '../../../services/api'
+
+interface IProduct {
+  id: number;
+  title: string;
+}
+
+interface CategoryProps {
+  products: IProduct[]
+}
+
+export default function Category({products}: CategoryProps) {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <p>Loading...</p>
+  }
+
+  return (
+    <div>
+       <h1>{router.query.slug}</h1>
+
+       <ul>
+          {products.map((product) => (
+            <li key={product.id}>{product.title}</li>
+          ))}
+        </ul>
+    </div>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('categories')
+
+  const paths = data.map((category: IProduct) => ({
+    params: {
+      slug: category.id
+    }
+  }))
+
+  return {
+    paths,
+    fallback: true
+  }
+}
+
+export const getStaticProps: GetStaticProps<CategoryProps> = async (context) => {
+  const {slug} = context.params;
+
+  const response = await api.get('products', {
+    params: {
+      category_id: slug,
+    }
+  })
+
+  const products = response.data
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60
+  }
+}
